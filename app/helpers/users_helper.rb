569a75869314b8beafe1bf0110ae62e827663df0
kassy11 <kotaro11kassy@gmail.com>
2019-11-19 18:51:17 +0900

    fix user images

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,3 @@
 module UsersHelper
-  # require 'rspotify'
 
-  # def find_artist(my_music)
-  #   RSpotify::Artist.find(my_music.artist_id)
-  # end
-
-  # 引数で与えられたユーザーのGravatar画像を返す
- def gravatar_for(user,size)
-   gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-   image_tag(gravatar_url, alt: user.name, class: "gravatar")
- end
 end
