2e34ad5941f4706c7e5b0ef6686985b9e8420fb0
kassy11 <kotaro11kassy@gmail.com>
2020-02-13 22:41:55 +0900

    ルーティングのリファクタリング９割完了

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -7,20 +7,20 @@ class MusicsController < ApplicationController
   end
 
   def show
-    @artist = find_artist(params[:artist_id])
+    @artist = find_artist(params[:work_id])
     @title = @artist.name
     @img_url = @artist.images[1]["url"] unless @artist.images.blank?
   end
 
   def create
-    @artist_fav = current_user.musics.new(artist_id: params[:artist_id])
+    @artist_fav = current_user.musics.new(artist_id: params[:work_id])
     @artist_fav.save
-    redirect_to my_music_path(current_user), notice: 'MUSIC LISTの項目を追加しました'
+    redirect_to music_index_user_path(current_user), notice: 'MUSIC LISTの項目を追加しました'
   end
 
   def destroy
-    @artist_fav = current_user.musics.find_by(artist_id: params[:artist_id])
+    @artist_fav = current_user.musics.find_by(artist_id: params[:work_id])
     @artist_fav.destroy
-    redirect_to my_music_path(current_user), alert: 'MOVIE LISTの項目を削除しました'
+    redirect_to music_index_user_path(current_user), alert: 'MOVIE LISTの項目を削除しました'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,21 @@
 Rails.application.routes.draw do
-  post "musics/:artist_id/create" => "musics#create",param: :artist_id, as: :create_music_list
-  post "musics/:artist_id/destroy" => "musics#destroy", param: :artist_id, as: :destroy_music_list
-
-  post "books/:book_id/create" => "books#create",param: :book_id, as: :create_book_list
-  post "books/:book_id/destroy" => "books#destroy", param: :book_id, as: :destroy_book_list
-
-  post "movies/:movie_id/create" => "movies#create",param: :movie_id, as: :create_movie_list
-  post "movies/:movie_id/destroy" => "movies#destroy", param: :movie_id, as: :destroy_movie_list
+  resources :musics, :books, :movies, only: [ :destroy, :show ], param: :work_id
+  post "musics/:work_id/create" => "musics#create", as: :musics
+  post "movies/:work_id/create" => "movies#create", as: :movies
+  post "books/:work_id/create" => "books#create", as: :books
+  ## ここの上下３つをどうにかまとめたい..
+  post 'musics/index' => 'musics#index'
+  post 'books/index' => 'books#index'
+  post 'movies/index' => 'movies#index'
+  # getに書き換える
 
 
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'users/:id' => 'users#show', as: :mypage
-
-  get 'users/:id/music_index' => 'users#music_index', as: :my_music
-  post 'musics/index' => 'musics#index'
-  resources :musics, only: :show, param: :artist_id
-
-  get 'users/:id/book_index' => 'users#book_index', as: :my_book
-  post 'books/index' => 'books#index'
-  resources :books, only: :show, param: :book_id
-
-  get 'users/:id/movie_index' => 'users#movie_index', as: :my_movie
-  post 'movies/index' => 'movies#index'
-  resources :movies, only: :show, param: :movie_id
+  resources :users, only: :show do
+    get :book_index, on: :member
+    get :music_index, on: :member
+    get :movie_index, on: :member
+  end
 
   root 'static_pages#home'
 end
