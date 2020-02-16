c3ba67645f3888f4f4e1751e0942675d4e378757
kassy11 <kotaro11kassy@gmail.com>
2020-02-16 14:48:33 +0900

    検索結果・フォーム画面をカードデザインに変更した

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -3,7 +3,6 @@ class MusicsController < ApplicationController
 
   def index
     @artists = search_artist(params[:search]) unless params[:search].blank?
-    binding.pry
     @base_contents = @artists
   end
 
