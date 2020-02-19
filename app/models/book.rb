1d51521fa6a527e8f38f6abc8b9c5d45b1904feb
kassy11 <kotaro11kassy@gmail.com>
2020-02-19 14:45:15 +0900

    同じ作品を追加できるエラーをバリデーションを設定して解決

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :user_id, {presence: true}
-  validates :book_id, {presence: true}
+  validates :book_id, {presence: true, uniqueness: true}
   belongs_to :user
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   validates :user_id, {presence: true}
-  validates :movie_id, {presence: true}
+  validates :movie_id, {presence: true, uniqueness: true}
   belongs_to :user
   API_KEY = Rails.application.credentials.moviedb[:api_key]
 end
