07ca138a07feb70191bea68f53b8f8d337d84d05
kassy11 <kotaro11kassy@gmail.com>
2020-02-19 13:47:14 +0900

    検索フォームをgetに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'musics/index' => 'musics#index'
+  get 'books/index' => 'books#index'
+  get 'movies/index' => 'movies#index'
+
   post "musics/:work_id/create" => "musics#create", as: :musics
   post "movies/:work_id/create" => "movies#create", as: :movies
   post "books/:work_id/create" => "books#create", as: :books
+  ## TODO:ここの上下３つをどうにかまとめたい..
+
   resources :musics, :books, :movies, only: [ :destroy, :show ], param: :work_id
 
-  ## ここの上下３つをどうにかまとめたい..
-  post 'musics/index' => 'musics#index'
-  post 'books/index' => 'books#index'
-  post 'movies/index' => 'movies#index'
-  # getに書き換える
 
 
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
