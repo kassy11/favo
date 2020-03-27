bb1a37e637a5ab54538cf84acdcdb3704f01b57f
kassy11 <kotaro11kassy@gmail.com>
2020-03-27 16:38:20 +0900

    :robot: users_controllerのテスト完了

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe "#home" do
-    context "as an authenticate user" do
+    context "as an authorized user" do
       before do
         @user = create(:user)
+      end
+
+      # rspecのバグでbe_successが使えないようなので一旦pending
+      xit 'response successfully' do
+        sign_in @user
+        get :home
+        expect(response).to be_successful
       end
 
       it 'return a 302 response' do
@@ -22,6 +29,11 @@ RSpec.describe StaticPagesController, type: :controller do
     end
 
     context "as a guest" do
+      xit 'response successfully' do
+        get :home
+        expect(response).to be_successful
+      end
+
       it 'return a 200 response' do
         get :home
         expect(response).to have_http_status "200"
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,206 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  shared_context 'as an authorized user' do
+    let(:user){create(:user)}
+  end
+
+  shared_context 'as an unauthorized user' do
+      let(:user){create(:user)}
+      let(:other_user){create(:user)}
+  end
+
+  shared_context 'as a guest' do
+    let!(:user){create(:user)}
+  end
+
+
+  describe '#show' do
+    context 'as an authorized user' do
+      include_context 'as an authorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :show, params: {id: user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :show, params: {id: user.id}
+        expect(response).to have_http_status "200"
+      end
+
+    end
+
+    context 'as an unauthorized user' do
+      include_context 'as an unauthorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :show, params: {id: other_user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :show, params: {id: other_user.id}
+        expect(response).to have_http_status "200"
+      end
+
+
+    end
+
+    context 'as a guest' do
+      include_context 'as a guest'
+
+      xit 'response successfully' do
+        get :show, params: {id: user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        get :show, params: {id: user.id}
+        expect(response).to have_http_status "200"
+      end
+    end
+
+  end
+
+  describe '#music_index' do
+    context 'as an authorized user' do
+      include_context 'as an authorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :music_index, params: {id: user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :music_index, params: {id: user.id}
+        expect(response).to have_http_status "200"
+      end
+
+    end
+
+    context 'as an unauthorized user' do
+      include_context 'as an unauthorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :music_index, params: {id: other_user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :music_index, params: {id: other_user.id}
+        expect(response).to have_http_status "200"
+      end
+
+    end
+
+    context 'as a guest' do
+      include_context 'as a guest'
+
+      xit 'response successfully' do
+        get :music_index, params: {id: user.id}
+        expect(response).to be_success
+      end
+    end
+
+  end
+
+  describe '#movie_index' do
+    context 'as an authorized user' do
+      include_context 'as an authorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :movie_index, params: {id: user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :movie_index, params: {id: user.id}
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    context 'as an unauthorized user' do
+      include_context 'as an unauthorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :movie_index, params: {id: other_user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :movie_index, params: {id: other_user.id}
+        expect(response).to have_http_status "200"
+      end
+
+    end
+
+    context 'as a guest' do
+      include_context 'as a guest'
+
+      xit 'response successfully' do
+        get :movie_index, params: {id: user.id}
+        expect(response).to be_success
+      end
+    end
+  end
+
+  describe 'book_index' do
+    context 'as an authorized user' do
+      include_context 'as an authorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :book_index, params: {id: user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :book_index, params: {id: user.id}
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    context 'as an unauthorized user' do
+      include_context 'as an unauthorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :book_index, params: {id: other_user.id}
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :book_index, params: {id: other_user.id}
+        expect(response).to have_http_status "200"
+      end
+
+    end
+
+    context 'as a guest' do
+      include_context 'as a guest'
+
+      xit 'response successfully' do
+        get :book_index, params: {id: user.id}
+        expect(response).to be_success
+      end
+    end
+
+  end
+
+end
