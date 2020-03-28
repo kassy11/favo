7edf8652fc880bfb92158c2b1184da7535833c25
kassy11 <kotaro11kassy@gmail.com>
2020-03-28 13:39:22 +0900

    :robot: feature-spec用のファイルと設定を追加

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  scenario 'user create a new music favorite' do
+
+  end
+
+  scenario 'user create a new movie favorite' do
+
+  end
+
+  scenario 'user create a new book favorite' do
+
+  end
+
+
+end
---
+++ spec/support/capybara.rb
@@ -0,0 +1,4 @@
+Capybara.javascript_driver=:selenium_chrome_headless
+
+Capybara.default_max_wait_time=15
+
