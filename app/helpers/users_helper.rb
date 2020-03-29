3a2c12bad93805074fc400a0f2ad81e17d24985a
kassy11 <kotaro11kassy@gmail.com>
2020-03-29 11:55:21 +0900

    image_tag変更

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@
 module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=250"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=300"
     gravatar_url
   end
 end
