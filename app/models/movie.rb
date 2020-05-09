dd346018dec5cf6c7352a9c9d7b34af0a15ec335
kassy11 <kotaro11kassy@gmail.com>
2020-05-09 12:16:07 +0900

    :pencil: 外部キー制約などのモデル関連の修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@
 class Movie < ApplicationRecord
   validates :user_id, { presence: true }
   validates :movie_id, { presence: true, uniqueness: { scope: :user_id } }
-  belongs_to :user
+  belongs_to :user, foreign_key: :user_id
   API_KEY = Rails.application.credentials.moviedb[:api_key]
   GOOGLE_API_KEY = Rails.application.credentials.google[:api_key]
 end
