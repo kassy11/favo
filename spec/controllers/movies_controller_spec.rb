8e7af2fbfd7c45ca253533d26ccdeabe55de64e6
kassy11 <kotaro11kassy@gmail.com>
2020-05-14 18:29:16 +0900

    :robot: 一旦コントローラスペック終わり

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -16,6 +16,9 @@ RSpec.describe MoviesController, type: :controller do
     let!(:user) { create(:user) }
   end
 
+  shared_context 'as an authorized user who has fav list' do
+    let(:user) { create(:user, :with_fav_movies) }
+  end
 
   describe '#search' do
     context 'as an authorized user' do
@@ -63,8 +66,8 @@ RSpec.describe MoviesController, type: :controller do
         expect(response).to have_http_status '200'
       end
 
-      # TODO: 検索結果の配列をどうテストすればいいのかわからないのでpending
-      # そもそもこれはcontroller specでやることではない？？
+      # 検索結果の配列をどうテストすればいいのかわからないのでpending
+      # TODO:そもそもこれはcontroller specではなくフィーチャースペック（Capybara）でやるのかも
       xit 'has search result items' do
         sign_in user
         get :index, params: { name: 'ドラえもん' }
@@ -119,34 +122,38 @@ RSpec.describe MoviesController, type: :controller do
     context 'as an authorized user' do
       include_context 'as an authorized user'
 
-      # TODO: 何故か作品が追加されない？？
-      xit 'adds a movie to user list' do
+      it 'adds a movie to user list' do
         sign_in user
-        movie = create(:movie, user: user)
-        binding.pry
+        movie = create(:movie)
         expect  do
           post :create, params: { work_id: movie.movie_id }
         end.to change(user.movies, :count).by(1)
       end
 
-      it 'redirect _path after adds a movie' do
+      it 'redirect movie_index_user_path after adds a movie' do
         sign_in user
         movie = create(:movie)
-        expect(post :create, params: { work_id: movie.id }).to redirect_to movie_index_user_path(user)
+        expect(post(:create, params: { work_id: movie.id })).to redirect_to movie_index_user_path(user)
+      end
+
+      # TODO: これもほぼバリデーションのテストなのでコントローラスペックに書くのは違う気がする
+      xit 'cannot add same work to user list' do
+        sign_in user
+        movie = create(:movie)
+        post :create, params: { work_id: movie.id }
+        expect(post(:create, params: { work_id: movie.id })).to be_falsey
       end
     end
 
     context 'as an unauthorized user' do
       include_context 'as an unauthorized user'
 
-      # TODO: そもそものバリデーションのかけ方がおかしいのかもしれない
-      # それともこれはテストしなくていい？？
-      xit 'does not add a movie to user list' do
+      it 'does not add a movie to user list' do
         sign_in user
-        movie = create(:movie, user: other_user)
+        movie = create(:movie)
         expect  do
           post :create, params: { work_id: movie.movie_id }
-        end.not_to change(user.movies, :count)
+        end.not_to change(other_user.movies, :count)
       end
     end
 
@@ -162,21 +169,22 @@ RSpec.describe MoviesController, type: :controller do
   end
 
   describe '#destory' do
-    context 'as an authorized user' do
-      include_context 'as an authorized user'
+    context 'as an authorized user who has fav list' do
+      include_context 'as an authorized user who has fav list'
 
-      # destroyがNoMethodErrorになってしまう謎のエラー
+      # TODO: destroyがnil呼び出しになってる（作品がないから）問題を解決する
       xit 'destroy a movie in user list' do
         sign_in user
-        movie = create(:movie, user: user)
-        p movie
+        movie = user.movies.first
         expect do
-          delete :destroy, params: { work_id: movie.id }
+          post :destroy, params: { work_id: movie.id }
         end.to change(user.movies, :count).by(-1)
       end
 
-      # redirectさせる方法がわからない、コールバック？？
       xit 'redirect movie_index_user_path after delete a movie' do
+        sign_in user
+        movie = create(:movie, user: user)
+        expect(post(:destroy, params: { work_id: movie.id })).to redirect_to movie_index_user_path(user)
       end
     end
 
@@ -194,6 +202,14 @@ RSpec.describe MoviesController, type: :controller do
     end
 
     context 'as a guest' do
+      xit 'does not destroy a movie in user list' do
+        sign_in user
+        movie = create(:movie, user: user)
+        p movie
+        expect do
+          delete :destroy, params: { work_id: movie.id }
+        end.to change(user.movies, :count).by(-1)
+      end
     end
   end
 end
--- spec/controllers/musics_controller_spec.rb
+++ spec/controllers/musics_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe MusicsController, type: :controller do
@@ -14,47 +16,8 @@ RSpec.describe MusicsController, type: :controller do
     let!(:user) { create(:user) }
   end
 
-  describe '#index' do
-
-  end
-
-  describe '#create' do
-
-  end
-
-  describe '#destroy' do
-
-  end
-
-  describe '#show' do
-    context 'as an authorized user' do
-      include_context 'as an authorized user'
-      ## ここでRSpotifyの認証エラーになってしまう
-      xit 'response successfully' do
-        sign_in user
-        get :show, params: {work_id: "2kQnsbKnIiMahOetwlfcaS".to_i }
-        expect(response).to be_successful
-      end
-
-      xit 'return a 200 response' do
-        sign_in user
-        get :show, params: {work_id: "2kQnsbKnIiMahOetwlfcaS"}
-        expect(response).to have_http_status '200'
-      end
-    end
-
-    context 'as a guest' do
-      include_context 'as a guest'
-      xit 'response successfully' do
-        get :show, params: { work_id: "2kQnsbKnIiMahOetwlfcaS"}
-        expect(response).to be_successful
-      end
-
-      xit 'return a 200 response' do
-        get :show, params: { work_id: "2kQnsbKnIiMahOetwlfcaS"}
-        expect(response).to have_http_status '200'
-      end
-    end
+  shared_context 'as an authorized user who has fav list' do
+    let(:user) { create(:user, :with_fav_musics) }
   end
 
   describe '#search' do
@@ -64,7 +27,7 @@ RSpec.describe MusicsController, type: :controller do
       xit 'response successfully' do
         sign_in user
         get :search
-        expect(response).to be_successful
+        expect(response).to be_success
       end
 
       it 'return a 200 response' do
@@ -75,14 +38,178 @@ RSpec.describe MusicsController, type: :controller do
     end
 
     context 'as a guest' do
-      xit 'redirect to new_user_session_path' do
-        get :search
-        expect(response).to redirect_to new_user_session_path
-      end
-
       it 'return a 302 response' do
         get :search
         expect(response).to have_http_status '302'
+      end
+
+      it 'redirect to login_path' do
+        get :search
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe '#index' do
+    context 'as an authorized user' do
+      include_context 'as an authorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :index, params: { name: 'oasis' }
+        expect(response).to be_success
+      end
+
+      it 'return a 200 response' do
+        sign_in user
+        get :index, params: { name: 'oasis' }
+        expect(response).to have_http_status '200'
+      end
+
+      # 検索結果の配列をどうテストすればいいのかわからないのでpending
+      # TODO:そもそもこれはcontroller specではなくフィーチャースペック（Capybara）でやるのかも
+      xit 'has search result items' do
+        sign_in user
+        get :index, params: { name: 'oasis' }
+        expect(response).to
+      end
+    end
+
+    context 'as a guest' do
+      it 'return a 302 response' do
+        get :index, params: { name: 'oasis' }
+        expect(response).to have_http_status '302'
+      end
+
+      it 'redirect to login_path' do
+        get :index, params: { name: 'oasis' }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  # TODO: AuthenticationErrorになるため、ここはモック・スタブをつくる
+  describe '#show' do
+    context 'as an authorized user' do
+      include_context 'as an authorized user'
+
+      xit 'response successfully' do
+        sign_in user
+        get :show, params: { work_id: 'MMh_tgEACAAJ' }
+        expect(response).to be_success
+      end
+
+      xit 'return a 200 response' do
+        sign_in user
+        get :show, params: { work_id: 'MMh_tgEACAAJ' }
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context 'as a guest' do
+      xit 'response successfully' do
+        get :show, params: { work_id: 265_712 }
+        expect(response).to be_success
+      end
+
+      xit 'return a 200 response' do
+        get :show, params: { work_id: 265_712 }
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe '#create' do
+    context 'as an authorized user' do
+      include_context 'as an authorized user'
+
+      it 'adds an artist to user list' do
+        sign_in user
+        music = create(:music)
+        expect  do
+          post :create, params: { work_id: music.artist_id }
+        end.to change(user.musics, :count).by(1)
+      end
+
+      it 'redirect music_index_user_path after adds an artist' do
+        sign_in user
+        music = create(:music)
+        expect(post(:create, params: { work_id: music.id })).to redirect_to music_index_user_path(user)
+      end
+
+      # TODO: これもほぼバリデーションのテストなのでコントローラスペックに書くのは違う気がする
+      xit 'cannot add same artists to user list' do
+        sign_in user
+        music = create(:music)
+        post :create, params: { work_id: music.id }
+        expect(post(:create, params: { work_id: music.id })).to be_falsey
+      end
+    end
+
+    context 'as an unauthorized user' do
+      include_context 'as an unauthorized user'
+
+      it 'does not add an artist to user list' do
+        sign_in user
+        music = create(:music)
+        expect  do
+          post :create, params: { work_id: music.artist_id }
+        end.not_to change(other_user.musics, :count)
+      end
+    end
+
+    context 'as a guest' do
+      include_context 'as a guest'
+      it 'does not add an artist to user list' do
+        music = create(:music, user: user)
+        expect  do
+          post :create, params: { work_id: music.id }
+        end.not_to change(user.musics, :count)
+      end
+    end
+  end
+
+  describe '#destory' do
+    context 'as an authorized user who has fav list' do
+      include_context 'as an authorized user who has fav list'
+
+      # TODO: destroyがnil呼び出しになってる（作品がないから）問題を解決する
+      xit 'destroy an artist in user list' do
+        sign_in user
+        music = user.musics.first
+        expect do
+          post :destroy, params: { work_id: music.id }
+        end.to change(user.musics, :count).by(-1)
+      end
+
+      xit 'redirect music_index_user_path after destroy an artist' do
+        sign_in user
+        music = create(:music, user: user)
+        expect(post(:destroy, params: { work_id: music.id })).to redirect_to music_index_user_path(user)
+      end
+    end
+
+    context 'as an unauthorized user' do
+      include_context 'as an unauthorized user'
+
+      xit 'does not destroy an artist in user list' do
+        sign_in user
+        music = create(:music, user: user)
+        p music
+        expect do
+          delete :destroy, params: { work_id: music.id }
+        end.to change(user.musics, :count).by(-1)
+      end
+    end
+
+    context 'as a guest' do
+      xit 'does not destroy an artist in user list' do
+        sign_in user
+        music = create(:music, user: user)
+        p music
+        expect do
+          delete :destroy, params: { work_id: music.id }
+        end.to change(user.musics, :count).by(-1)
       end
     end
   end
