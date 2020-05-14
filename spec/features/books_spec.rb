61e17c52b3e8b4f1b8d287a3c7d915a613d5fbec
kassy11 <kotaro11kassy@gmail.com>
2020-05-14 21:18:13 +0900

    :robot: bookとmovieの正常系の統合テストもおわり

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -3,5 +3,29 @@
 require 'rails_helper'
 
 RSpec.feature 'Books', type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario 'user search books and creates a new book favorite' do
+    user = create(:user)
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'Eメール', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+
+    expect{
+      within '.book-fav-btns' do
+        click_link('追加する')
+      end
+
+      fill_in 'search-form-id', with: '村上春樹'
+      click_button '検索'
+
+      expect(page).to have_content('村上春樹の検索結果')
+      first(".list-btns").click_link('詳細')
+      expect(page).to have_content('著者')
+      click_link('リストに追加する')
+
+    }.to change(user.books, :count).by(1)
+
+  end
 end
--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -3,5 +3,29 @@
 require 'rails_helper'
 
 RSpec.feature 'Movies', type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario 'user search movies and creates a new movie favorite' do
+    user = create(:user)
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'Eメール', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+
+    expect{
+      within '.movie-fav-btns' do
+        click_link('追加する')
+      end
+
+      fill_in 'search-form-id', with: 'ドラえもん'
+      click_button '検索'
+
+      expect(page).to have_content('ドラえもんの検索結果')
+      first(".list-btns").click_link('詳細')
+      expect(page).to have_content('ジャンル')
+      click_link('リストに追加する')
+
+    }.to change(user.movies, :count).by(1)
+
+  end
 end
